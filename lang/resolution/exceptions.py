ebfe4ce84a11950377edf8536ffdc8b8cf0c03bd
Alekseypavlov14 <aleshapavlov9@gmail.com>
2025-06-04 09:34:34 +0300

    added topological_sort to registry

--- lang/resolution/exceptions.py
+++ lang/resolution/exceptions.py
@@ -8,3 +8,7 @@ class ModuleError(Exception):
   def __init__(self, message: str):
     super().__init__(message)
     
+# use for resolution error (during topological sort)
+class ResolutionError(Exception):
+  def __init__(self, message: str):
+    super().__init__(message)
--- lang/resolution/registry.py
+++ lang/resolution/registry.py
@@ -1,4 +1,5 @@
 from resolution.module import *
+from resolution.exceptions import *
 
 # this class is used to contain application modules (files)
 # it provides utils for topological sort based on dependencies
@@ -43,4 +44,85 @@ class Registry:
 
   # sorts modules topologically in place
   def sort_topologically(self):
-    pass
+    # set of all module paths in registry
+    undiscovered: set[str] = set(map(lambda module: module.path, self.modules))
+    # set of module paths that are discovered but not analyzed
+    discovered = set()
+    # set of module paths that are analyzed 
+    analyzed = set()
+
+    # list of nodes in topologically sorted order
+    sorted = []
+
+    def depth_first_search(module: Module):
+      # if cycle is completed and depth-first search reached discovered node
+      if module.path in discovered:
+        raise ResolutionError(f'Circular dependency including module by path {module.path}')
+      
+      # if module is analyzed
+      if module.path in analyzed:
+        # do not analyze this module again
+        return
+
+      # discover node
+      undiscovered.remove(module.path)
+      discovered.add(module.path)
+
+      # discover all successors
+      for dependency in module.dependencies:
+        depth_first_search(dependency)
+
+      # analyze node (all dependencies are analyzed)
+      discovered.remove(module.path)
+      analyzed.add(module.path)
+
+      # add module to sorted
+      sorted.append(module)
+
+    # in_degree is needed to start DFS only from 0 in-degree nodes (requirement of algorithm)
+    modules_in_degree: list[int] = []
+
+    # compute in-degree for each module
+    for module in self.modules:
+      # compute in_degree
+      in_degree = 0
+
+      # analyze potential dependent modules
+      for dependent in self.modules:
+        # skip current module
+        if dependent.path == module.path:
+          continue
+        
+        # compute list of dependency paths
+        dependency_paths = []
+
+        # fill dependency_paths
+        for dependency in dependent.dependencies:
+          dependency_paths.append(dependency.path)
+
+        # increment in_degree if needed
+        if module.path in dependency_paths:
+          in_degree += 1
+
+      modules_in_degree.append(in_degree)
+
+    # execute DFS
+    for module_index in range(len(self.modules)):
+      # check for zero in_degree
+      if modules_in_degree[module_index] == 0: 
+        depth_first_search(self.modules[module_index])
+
+    # check if DFS is executed
+    # if registry contains modules but no module was analyzed and added to sorted
+    # Indicates that every module has in-degree. It indicates Circular Dependency
+    if len(self.modules) and not len(sorted):
+      raise ResolutionError('Circular dependency in modules. Every module is dependent on others')
+
+    # reverse list to have dependencies at the beginning and dependents at the end
+    sorted.reverse()
+
+    # update modules field with sorted modules
+    self.modules = sorted
+
+    return sorted
+  
