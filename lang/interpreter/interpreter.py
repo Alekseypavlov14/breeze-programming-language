350f3552e8a4ff5657fbafe9c8dc0962e9373c56
Alekseypavlov14 <aleshapavlov9@gmail.com>
2025-06-08 17:47:37 +0300

    started interpreter class

--- lang/interpreter/interpreter.py
+++ lang/interpreter/interpreter.py
@@ -1,3 +1,25 @@
+from interpreter.stack import *
+from resolution.module import *
+
 # executes AST
 class Interpreter:
-  pass
+  def __init__(self):
+    # modules of application
+    self.modules: list[Module] = []
+    # list of stacks for each module
+    self.stacks: list[Stack] = []
+
+    # alias for current executing module
+    # order of execution is defined by Resolver
+    current_module: Module | None = None
+    # alias for current stack
+    # using imported functions requires switching the stack
+    current_stack: Stack | None = None
+
+  # method to load app modules to application
+  # creates stack for each module
+  def load_modules(self, modules: list[Module]):
+    # set modules list
+    self.modules = modules
+    # create stacks for each module
+    self.stacks = [Stack() for _ in modules]
